f49b63aa079f21ec93d1c86b0679a34fa78904a7
Hydrong <chickchok06@gmail.com>
2026-06-26 17:09:30 +0900

    프로젝트 생성

---
+++ src/main/kotlin/com/moijang/moijangbackend/MoiJangBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.moijang.moijangbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MoiJangBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<MoiJangBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/moijang/moijangbackend/MoiJangBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.moijang.moijangbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoiJangBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
