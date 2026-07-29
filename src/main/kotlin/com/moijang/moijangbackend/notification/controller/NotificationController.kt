885ebd75e45843153f2e93b15a37c895383fd379
Hydrong <chickchok06@gmail.com>
2026-07-29 16:47:27 +0900

    Notification API 이름 추가 (스칼라용)

--- src/main/kotlin/com/moijang/moijangbackend/notification/controller/NotificationController.kt
+++ src/main/kotlin/com/moijang/moijangbackend/notification/controller/NotificationController.kt
@@ -2,10 +2,12 @@ package com.moijang.moijangbackend.notification.controller
 
 import com.moijang.moijangbackend.global.common.ApiResponse
 import com.moijang.moijangbackend.notification.dto.NotificationDto
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "Notification API")
 @RestController
 @RequestMapping("/api/v1/notifications")
 class NotificationController {
